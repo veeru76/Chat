8898f27ef7a80fa3e4b557d9b6c90747ceed3c5b
veeru <veeranjaneyaveeru@gmail.com>
2022-07-23 23:42:46 +0530

    created chat and emitting and broadcasting message

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 'use strict';
+const path = require('path');
 const express = require('express');
-const app = express();
 const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
 const server = http.createServer(app);
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+const io = new Server(server);
+
+const publicDirPath = path.join(__dirname + '/public');
+app.use(express.static(publicDirPath));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.emit('message', 'welcome to chat');
+  socket.broadcast.emit('message', 'a new user has joined');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('message', 'a user has left the chat');
+  });
 });
 server.listen(3000, () => {
-  // eslint-disable-next-line no-console
   console.log('listening on *:3000');
 });
